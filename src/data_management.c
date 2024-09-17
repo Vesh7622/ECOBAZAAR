45d98993efdb19074146cd604e72d7bd8acfd926
Nirvesh Bhagirath <nirvesh.bhagirath1@umail.uom.ac.mu>
2024-09-17 20:20:32 +0400

    addition of mew files

---
+++ src/data_management.c
@@ -0,0 +1,9 @@
+#include "data_management.h"
+
+void loadData() {
+    // Implementation for loading data
+}
+
+void saveData() {
+    // Implementation for saving data
+}
---
+++ src/data_management.h
@@ -0,0 +1,2 @@
+void loadData();
+void saveData();
