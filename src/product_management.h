75d9513cae56ab0d54f27a402054cf2eb1281b92
keshav <keshavdabeedyal08@gmail.com>
2024-10-11 13:38:50 +0400

    correction

--- src/product_management.h
+++ src/product_management.h
@@ -13,10 +13,12 @@ typedef struct {
 extern Product products[];    // Declare products as an array of Product structs
 extern int product_count;     // Declare product_count
 
+// Function prototypes
 void load_products();
 void save_products();
 void add_product(char *name, double price, int stock);
 void sell_product(int product_id, int quantity);
 void list_products();
-
+void browse_products_screen();  // Add the declaration for browse_products_screen
 #endif
+
--- src/user_management.h
+++ src/user_management.h
@@ -2,10 +2,21 @@
 #ifndef USER_MANAGEMENT_H
 #define USER_MANAGEMENT_H
 
+// Define the User structure
+typedef struct {
+    char username[50];
+    char password[50];
+} User;
+
+// Declare users and user_count as extern so other modules can access them
+extern User users[];    // Declare users as an array of User structs
+extern int user_count;  // Declare user_count
+
+// Function prototypes
 void load_users();
 void save_users();
-int login_user(char *username, char *password, char *role);
-int register_user(char *username, char *password, char *role);
+int login_user(char *username);  // Login function
+int register_user(char *username);  // Register function
 
 #endif
 
