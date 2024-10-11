a9303472945fe8e0cd69c2a993ea0d45d9ec36d2
KrishnaRuch <krishna.ruchchan2@umail.uom.ac.mu>
2024-10-11 13:36:54 +0400

    updated header file

--- src/shopping_cart.h
+++ src/shopping_cart.h
@@ -3,15 +3,17 @@
 
 // Declare cart_count globally
 extern int cart_count;
+extern double total_price;  // Declare total_price globally to track total price of cart
 
+// Function prototypes
 void load_cart(const char *username);
 void save_cart();
 void add_to_cart(int product_id, int quantity);
 void modify_cart_quantity(int product_id, int new_quantity);
-void remove_from_cart(int product_id);
 void view_cart();
 void checkout();
 
 #endif
+
 
 
